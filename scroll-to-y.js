d7c91a01deb8f3920907afdb95c6722c4940b19f
Bogdan Chadkin <trysound@yandex.ru>
2015-09-11 09:16:07 +0300

    Stop animation on wheel and keydown

--- scroll-to-y.js
+++ scroll-to-y.js
@@ -1,14 +1,40 @@
 // http://stackoverflow.com/questions/8917921/cross-browser-javascript-not-jquery-scroll-to-top-animation
 // first add raf shim
 // http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
-var requestAnimFrame = (function(){
-  return  global.requestAnimationFrame       ||
-          global.webkitRequestAnimationFrame ||
-          global.mozRequestAnimationFrame    ||
-          function( callback ){
-            global.setTimeout(callback, 1000 / 60);
-          };
-})();
+var setTimeout = global.setTimeout;
+var requestAnimFrame = global.requestAnimationFrame       ||
+                       global.webkitRequestAnimationFrame ||
+                       global.mozRequestAnimationFrame    ||
+                       function( callback ){
+                           return setTimeout(callback, 1000 / 60);
+                       };
+
+var cancelAnimFrame = global.cancelAnimationFrame       ||
+                      global.webkitCancelAnimationFrame ||
+                      global.mozCancelAnimationFrame    ||
+                      global.clearTimeout;
+
+var easingEquations = {
+    easeOutSine: function (pos) {
+        return Math.sin(pos * (Math.PI / 2));
+    },
+    easeInOutSine: function (pos) {
+        return (-0.5 * (Math.cos(Math.PI * pos) - 1));
+    },
+    easeInOutQuint: function (pos) {
+        if ((pos /= 0.5) < 1) {
+            return 0.5 * Math.pow(pos, 5);
+        }
+        return 0.5 * (Math.pow((pos - 2), 5) + 2);
+    }
+};
+
+var wheelEvent;
+if ('onwheel' in global.document.createElement('a')) {
+    wheelEvent = 'wheel';
+} else if ('onmousewheel' in global.document.createElement('a')) {
+    wheelEvent = 'mousewheel';
+}
 
 // main function
 var scrollToY = function(scrollTargetY, speed, easing) {
@@ -16,51 +42,52 @@ var scrollToY = function(scrollTargetY, speed, easing) {
     // speed: time in pixels per second
     // easing: easing equation to use
 
-    var scrollY = global.scrollY,
-        currentTime = 0;
+    var scrollTo = global.scrollTo,
+        scrollY = global.scrollY,
+        currentTime = 0,
+        animId;
 
     scrollTargetY = scrollTargetY || 0;
     speed = speed || 2000;
-    easing = easing || 'easeOutSine';
+    easing = easingEquations[easing] || easingEquations['easeOutSine'];
 
     // min time .1, max time .8 seconds
     var time = Math.max(0.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8));
 
     // easing equations from https://github.com/danro/easing-js/blob/master/easing.js
-    var PI_D2 = Math.PI / 2,
-        easingEquations = {
-            easeOutSine: function (pos) {
-                return Math.sin(pos * (Math.PI / 2));
-            },
-            easeInOutSine: function (pos) {
-                return (-0.5 * (Math.cos(Math.PI * pos) - 1));
-            },
-            easeInOutQuint: function (pos) {
-                if ((pos /= 0.5) < 1) {
-                    return 0.5 * Math.pow(pos, 5);
-                }
-                return 0.5 * (Math.pow((pos - 2), 5) + 2);
-            }
-        };
+    var PI_D2 = Math.PI / 2;
 
     // add animation loop
     function tick() {
         currentTime += 1 / 60;
 
         var p = currentTime / time;
-        var t = easingEquations[easing](p);
+        var t = easing(p);
 
         if (p < 1) {
-            requestAnimFrame(tick);
+            animId = requestAnimFrame(tick);
 
-            global.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
+            scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
         } else {
-            global.scrollTo(0, scrollTargetY);
+            scrollTo(0, scrollTargetY);
         }
     }
 
     // call it once to get started
     tick();
+
+    // Stop on wheel or keydown
+    global.addEventListener(wheelEvent, function wheelListener(e) {
+        global.removeEventListener(wheelEvent, wheelListener);
+        cancelAnimFrame(animId);
+    });
+
+    global.addEventListener('keydown', function wheelListener(e) {
+        if ([ 38, 40, 32, 33, 34, 35, 36 ].indexOf(e.keyCode) !== -1) {
+            global.removeEventListener('keydown', wheelListener);
+            cancelAnimFrame(animId);
+        }
+    });
 };
 
 module.exports = scrollToY;
